fefbb0395432886bb7e600d016567b57d148c680
Kristianto <inikristiantoitu@gmail.com>
2025-08-07 18:47:55 +0700

    (hero text & layout) - Refactor app structure: remove constants, add Hero section and Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Hero from './sections/Hero';
 
 const App = () => {
-	return <div className='text-9xl'>App</div>;
+	return (
+		<>
+			<Hero />
+		</>
+	);
 };
 
 export default App;
+
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,56 @@
+import Button from '../components/button';
+import { words } from '../constants';
+
+function Hero() {
+	return (
+		<section
+			id='hero'
+			className='overflow-hidden relative'
+		>
+			<div className='top-0 left-0 z-10 absolute'>
+				<img
+					src='/images/bg.png'
+					alt='background'
+				/>
+			</div>
+
+			<div className='hero-layout'>
+				<header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+					<div className='flex flex-col gap-7'>
+						<div className='hero-text'>
+							<h1>
+								Shaping{' '}
+								<span className='slide'>
+									<span className='wrapper'>
+										{words.map((word) => (
+											<span
+												key={word.text}
+												className='flex items-center gap-1 md:gap-3 pb-2'
+											>
+												<img
+													src={word.imgPath}
+													alt={word.text}
+													className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+												/>
+												<span>{word.text}</span>
+											</span>
+										))}
+									</span>
+								</span>
+							</h1>
+							<h1>into Real Project</h1>
+							<h1>that Deliver Results</h1>
+						</div>
+						<p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>
+							Hi, I'am Kris, a Developer based in Indonesia with a
+							passion at creating something
+						</p>
+						<Button className='md:w-80 md:h-16 w-60 h-12' id='button' text='See my Work'/>
+					</div>
+				</header>
+			</div>
+		</section>
+	);
+}
+
+export default Hero;
