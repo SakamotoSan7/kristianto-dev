b704062c01ee8bb455da0f7bf099a793c60742c5
Kristianto <inikristiantoitu@gmail.com>
2025-09-01 21:13:54 +0700

    (Showcase) - Add ShowcaseSection component with GSAP animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from './sections/Hero';
+import ShowcaseSection from './sections/ShowcaseSection';
 
 const App = () => {
 	return (
 		<>
 			<Hero />
+
+			<ShowcaseSection/>
 		</>
 	);
 };
