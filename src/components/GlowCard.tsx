cbdabcaf457a9670a12cb518f06bc920bc1efc50
Kristianto <inikristiantoitu@gmail.com>
2025-09-03 19:52:15 +0700

    (testimonials) - Fix GlowCard component to use self-closing tag for glow div

--- src/components/GlowCard.tsx
+++ src/components/GlowCard.tsx
@@ -32,7 +32,7 @@ const GlowCard = ({ card, index, children }) => {
 			onMouseMove={handleMouseMove(index)}
 			className='card card-border timeline-card rounded-xl p-10 mb-5 break-inside-avoid-column'
 		>
-			<div className='glow'></div>
+			<div className='glow' />
 			<div className='flex items-center gap-1 mb-5'>
 				{Array.from({ length: 5 }, (_, i) => (
 					<img
